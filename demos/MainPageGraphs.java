6d359f07c620adf2b4dacd50f4f3167d2fbfbb81
abc123me <jeremiah_abc123me@hotmail.com>
2017-05-11 17:28:15 -0400

    Updated graphs

--- demos/MainPageGraphs.java
+++ demos/MainPageGraphs.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import net.net16.jeremiahlowe.scicalc.Enums.HorizontalAllignment;
 import net.net16.jeremiahlowe.scicalc.Enums.VerticalAllignment;
 import net.net16.jeremiahlowe.scicalc.cartesian_plane.CoordinatePlane;
@@ -7,7 +9,7 @@ import net.net16.jeremiahlowe.scicalc.functions.std.UnaryFunction;
 public class MainPageGraphs {
 	public static void main(String[] args) throws Exception{
 		CoordinatePlane cp = PlaneFactory.makePlaneWindow2D();
-		cp.clearAllFunctions();
+		
 		UnaryFunction uf = new UnaryFunction() {
 			@Override
 			public double f(double x) {
@@ -22,9 +24,27 @@ public class MainPageGraphs {
 		uf.getFunctionLabel().vAllign = allign2;
 		HorizontalAllignment allign = HorizontalAllignment.Custom;
 		allign.setCustomPercent(0.35d);
-		uf.getFunctionLabel().hAllign = allign;;
+		uf.getFunctionLabel().hAllign = allign;
+		
+		UnaryFunction uf2 = new UnaryFunction() {
+			@Override
+			public double f(double x) {
+				return Math.cos(x);
+			}
+		};
+		cp.addFunction(uf2);
+		uf2.setLineWidth(2);
+		uf2.getFunctionLabel().label = "cos x";
+		uf2.setColor(Color.BLUE);
+		VerticalAllignment allign4 = VerticalAllignment.Custom;
+		allign4.setCustomPercent(0.63d);
+		uf2.getFunctionLabel().vAllign = allign4;
+		HorizontalAllignment allign3 = HorizontalAllignment.Custom;
+		allign.setCustomPercent(0.35d);
+		uf2.getFunctionLabel().hAllign = allign3;
+		
 		cp.recalculate();
 		cp.takeScreenshot("demos/images/sine-main-page.png", "png");
-		System.exit(0);
+		//System.exit(0);
 	}
 }
