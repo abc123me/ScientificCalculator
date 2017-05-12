08295c1a9554853dfe0709b8265eb2145e60a1e0
abc123mewot <abc123mewot@localhost>
2017-05-12 08:49:44 -0400

    Removed testing classes

--- src/net/net16/jeremiahlowe/scicalc/testing/Whatever.java
+++
@@ -1,8 +0,0 @@
-package net.net16.jeremiahlowe.scicalc.testing;
-
-public class Whatever {
-	public static void main(String[] args) {
-		int[] numbers = {0, 1, 2, 3, 4, 5};
-		numbers[6] = 4;
-	}
-}
