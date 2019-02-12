eccb6e6d40019f68140a5751106036c4fc64f460
jeremiah <jeremiah@t430>
2019-02-12 10:17:09 -0500

    Added Javadocs to PlanePaintHandler

--- src/net/net16/jeremiahlowe/scicalc/cartesian_plane/PlanePaintHandler.java
+++ src/net/net16/jeremiahlowe/scicalc/cartesian_plane/PlanePaintHandler.java
@@ -3,7 +3,28 @@ package net.net16.jeremiahlowe.scicalc.cartesian_plane;
 import java.awt.Graphics;
 
 public interface PlanePaintHandler {
-	public void paint(CoordinatePlane cp, Graphics g);
+	/**
+	 * Called after the background is drawn, but before the axis, points, functions, and labels are drawn
+	 * Lifecycle: Enter, PrePaint, Axis grid, Points, Functions, Paint, Axes, PostPaint
+	 *
+	 * @param cp The CoordinatePlane object holding the PaintHandler
+	 * @param g The Graphics object being painted to
+	 */
 	public void prePaint(CoordinatePlane cp, Graphics g);
+	/**
+	 * Called after prePaint(), and functions, labels, points, etc.
+	 * Lifecycle: Enter, PrePaint, Axis grid, Points, Functions, Paint, Axes, PostPaint, Exit
+	 * 
+	 * @param cp The CoordinatePlane object holding the PaintHandler
+	 * @param g The Graphics object being painted to
+	 */
+	public void paint(CoordinatePlane cp, Graphics g);
+	/**
+	 * Called after a call to paint() and the axis are drawn;
+	 * Lifecycle: Enter, PrePaint, Axis grid, Points, Functions, Paint, Axes, PostPaint, Exit
+	 * 
+	 * @param cp The CoordinatePlane object holding the PaintHandler
+	 * @param g The Graphics object being painted to
+	 */
 	public void postPaint(CoordinatePlane cp, Graphics g);
 }
